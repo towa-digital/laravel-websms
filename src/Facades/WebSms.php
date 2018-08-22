ebf026bbb3e14baf21e4808426724b996e6db582
Aaron Schmied <aaron@pro-sales.ch>
2018-08-22 18:09:59 +0200

    Updated the docblocks for the websms facade.

--- src/Facades/WebSms.php
+++ src/Facades/WebSms.php
@@ -3,12 +3,17 @@
 namespace ProSales\WebSms\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use ProSales\WebSms\Contracts\Message;
+use ProSales\WebSms\Messages\SmsMessage;
 use ProSales\WebSms\WebSmsClient;
 
 /**
  * Class WebSms
  *
  * Provides facade access to the web sms client
+ *
+ * @method SmsMessage   smsMessage()            Create a new SMS message
+ * @method void         send(Message $message)  Send a given message
  *
  * @author Aaron Schmied <aaron@pro-sales.ch>
  * @package ProSales\WebSms\Facades
